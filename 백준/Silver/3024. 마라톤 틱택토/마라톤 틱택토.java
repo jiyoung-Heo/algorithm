51a071b6ab8727adad234f8eef80c4bb1a66f155
jiyoung-Heo <93931316+jiyoung-Heo@users.noreply.github.com>
2024-02-29 00:01:42 +0900

    [Silver III] Title: 마라톤 틱택토, Time: 156 ms, Memory: 16176 KB -BaekjoonHub

---
+++ 백준/Silver/3024. 마라톤 틱택토/마라톤 틱택토.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[] dx = { 0, 1, 1, 1 };
+		int[] dy = { 1, 1, 0, -1 };
+
+		char[][] map = new char[N][];
+
+		for (int i = 0; i < N; i++) {
+			map[i] = br.readLine().toCharArray();
+		}
+
+		String ans = "ongoing";
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				char tmp = map[i][j];
+				if (tmp != '.') {
+					for (int k = 0; k < dx.length; k++) {
+						int cnt = 0;
+						for (int l = 1; l < 3; l++) {
+							int nx = i + (dx[k] * l);
+							int ny = j + (dy[k] * l);
+							if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
+								break;
+							}
+							if (tmp == map[nx][ny]) {
+								cnt++;
+							} else {
+								break;
+							}
+						}
+						if (cnt == 2) {
+							System.out.println(String.valueOf(tmp));
+							System.exit(0);
+						}
+					}
+				}
+			}
+		}
+		System.out.println(ans);
+	}
+}
