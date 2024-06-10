9592a9c5a1c51b01ce04d7d528e3245ac864d9db
jiyoung-Heo <93931316+jiyoung-Heo@users.noreply.github.com>
2024-06-10 23:25:53 +0900

    [Silver IV] Title: 동전 0, Time: 80 ms, Memory: 11568 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (k == 0) break;
+            int temp = k / arr[i];
+            count += temp;
+            k = k % arr[i];
+        }
+        System.out.println(count);
+
+    }
+}
