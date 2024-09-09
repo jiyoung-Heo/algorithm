e249b79cf8271e06ab08ba46c71625adc7fbd80f
jiyoung-Heo <93931316+jiyoung-Heo@users.noreply.github.com>
2024-09-10 00:20:56 +0900

    [Bronze V] Title: 구구단, Time: 60 ms, Memory: 11436 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(num + " * "+i+" = "+(num*i));
+        }
+    }
+}
