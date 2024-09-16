0219a065e4f801f909d23767b14903ca09b16482
jiyoung-Heo <93931316+jiyoung-Heo@users.noreply.github.com>
2024-09-16 22:36:30 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 204 ms, Memory: 16780 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            // 공백 출력
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // 별 출력
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        sc.close();
+    }
+}
